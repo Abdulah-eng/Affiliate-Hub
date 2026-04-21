3ce099266b50c7977246ccd05a31f89664797c0b
hireitspecialist <hireitspecialist@gmail.com>
2026-04-22 01:17:07 +0500

    fix: import getImageSrc in SafeImage component

--- src/components/ui/SafeImage.tsx
+++ src/components/ui/SafeImage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { cn } from '@/lib/utils';
+import { cn, getImageSrc } from '@/lib/utils';
 
 interface SafeImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   fallback?: string;
